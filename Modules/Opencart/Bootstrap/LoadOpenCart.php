ab954f28535c88c29b9c9b52774433dd155c54ba
scil <scil.zc@gmail.com>
2020-04-23 10:06:25 +0800

    improve LoadOpenCart; add comments

--- Modules/Opencart/Bootstrap/LoadOpenCart.php
+++ Modules/Opencart/Bootstrap/LoadOpenCart.php
@@ -18,47 +18,47 @@ class LoadOpenCart
      */
     public function bootstrap(Application $app)
     {
-        $this->loadAdminConfig($app);
+        $this->loadConfig($app);
     }
 
-    protected function loadAdminConfig(\Illuminate\Foundation\Application $app)
+    protected function loadConfig(\Illuminate\Foundation\Application $app)
     {
         $opencart_path = realpath(env('OPENCART_DIR'));
-        if (!$opencart_path) {
-            $this->writeErrorAndDie("env 'OPENCART_DIR' does not exist");
+        if (!($opencart_path && is_file("$opencart_path/composer.json"))) {
+            $this->writeMsgOrDie("env 'OPENCART_DIR' does not exist");
         }
 
-        $opencart_admin_path = realpath(env('OPENCART_ADMIN_DIR'));
-        if (!$opencart_admin_path) {
-            $this->writeErrorAndDie("env 'OPENCART_ADMIN_DIR' does not exist");
+        $opencart_app_path = realpath(env('OPENCART_APPLICATION_DIR'));
+        if (!$opencart_app_path) {
+            $this->writeMsgOrDie("env 'OPENCART_APPLICATION_DIR' does not exist");
         }
 
 
         try {
-            require_once("$opencart_admin_path/config.php");
+            require_once("$opencart_app_path/config.php");
         } catch (\Exception $e) {
-            $this->writeErrorAndDie("env('OPENCART_ADMIN_DIR') does not exist");
+            $this->writeMsgOrDie("env('OPENCART_APPLICATION_DIR') does not exist");
         }
 
         $this->checkMysqlExtension($app);
 
-        $this->bindPathsInContainer($app, $opencart_path, $opencart_admin_path);
+        $this->bindInContainer($app, $opencart_path, $opencart_app_path);
     }
 
     protected function checkMysqlExtension(\Illuminate\Foundation\Application $app)
     {
         if (!defined('DB_DRIVER'))
-            $this->writeErrorAndDie('Const DB_DRIVER should be available.');
+            $this->writeMsgOrDie('Const DB_DRIVER should be available.');
 
 
-	$db = array(
-		'mysql', 
-		'mysqli', 
-		'pgsql', 
-		'pdo'
-	);
-        if (!in_array(DB_DRIVER, $db)) 
-            $this->writeErrorAndDie('DatabaseServiceProvider.php should update in '
+        $db = array(
+            'mysql',
+            'mysqli',
+            'pgsql',
+            'pdo'
+        );
+        if (!in_array(DB_DRIVER, $db))
+            $this->writeMsgOrDie('DatabaseServiceProvider.php should update in '
                 . ' Modules\Opencart\Providers, because OpenCart uses DB_DRIVER '
                 . DB_DRIVER
                 . ', instendof '
@@ -66,23 +66,41 @@ class LoadOpenCart
             );
 
         if (!extension_loaded('pdo_mysql'))
-            $this->writeErrorAndDie('Php ext pdo_mysql not loaed, so you can not access db from Laravel.',
+            $this->writeMsgOrDie('Php ext pdo_mysql not loaed, so you can not access db from Laravel.',
                 false);
 
     }
 
-    protected function bindPathsInContainer(Application $app, $opencart_path, $opencart_admin_path)
+    protected function bindInContainer(Application $app, $opencart_path, $opencart_app_path)
     {
-        $app->instance('path.opencart', $opencart_path);
-        $app->instance('path.opencartadmin', $opencart_admin_path);
+        // DIR_CATALOG only defined in admin/config.php
+        $is_admin = defined('DIR_CATALOG');
+
+        // this check would invalid if laravel and OpenCart server run in different os, like windows and virtual host
+//        if($is_admin && (DIR_APPLICATION !== $opencart_app_path)){
+//            $this->writeMsgOrDie('DIR_APPLICATION in config.php of admin should is '
+//                . $opencart_app_path
+//            );
+//        }
+
+        $this->writeMsgOrDie('OpenCart Application: '
+            . ($is_admin ? 'admin' : 'catalog'),
+            false);
+
+        $app->instance('opencart.admin', $is_admin);
+
+        $app->instance('opencart.path', $opencart_path);
+        $app->instance('opencart.apppath', $opencart_app_path);
+        $app->instance('opencart.adminpath', $is_admin ? $opencart_app_path : null);
+
     }
 
     /**
      * @param \Exception|string $e
      *
-     * copy from: Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables
+     * copy from: Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::writeErrorAndDie
      */
-    protected function writeErrorAndDie($e,$die=true)
+    protected function writeMsgOrDie($e, $die = true)
     {
         $output = (new ConsoleOutput)->getErrorOutput();
 
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 Access and control OpenCart 3 from Laravel.
 
-# Start
+Thanks for [nwidart/laravel-modules](https://github.com/nWidart/laravel-modules).
+
+# Features
+
+- use OpenCart front or admin config by add a bootstrap `Modules\Opencart\Bootstrap\LoadOpenCart.php`
+- bind something to Container: `opencart.admin`, `opencart.path`, `opencart.apppath`, and `opencart.adminpath`.
+- Communicate with opencart database with Laravel DB builder and Elequent.  how? to define some Model classes for opencart database.
+
+# Env
 
 edit .env
